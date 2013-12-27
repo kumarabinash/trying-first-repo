fc69d7029028015e60468454b35414b8e1bca6c2
Kumar Abinash <abinash.b2@gmail.com>
2013-12-27 15:01:08 +0530

    Added content to file.php

--- file.php
+++ file.php
@@ -14,5 +14,9 @@
 	<?php 
 		phpinfo();
 	?>
+	<p>
+		This is the fake content of the website to test, how its getting set up in github.com remote repository.
+		Lets commit.
+	</p>
 </body>
 </html>
