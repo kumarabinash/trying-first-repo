8f5be8a97de26b18143aac83a36b45d2980aea97
Kumar Abinash <abinash.b2@gmail.com>
2013-12-27 14:38:01 +0530

    added styling to file.php

--- file.php
+++ file.php
@@ -2,6 +2,13 @@
 <html>
 <head>
 	<title>This is a test for git.</title>
+	<style type="text/css">
+		body{
+			background-color: #888;
+			font-family: Helvetica;
+			color : #333;
+		}
+	</style>
 </head>
 <body>
 	<?php 
